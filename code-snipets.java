e7c1a45c0a411ea6ec067e5b2d5a6bef22526bac
Jemo Mghebrishvili <jemo.mgebrishvili@gmail.com>
2016-02-15 18:39:29 +0400

    GET request using Volley

---
+++ code-snipets.java
@@ -0,0 +1,27 @@
+
+
+// Simple GET request using Volley
+
+    private JsonArrayRequest jsonArrayRequest;
+    private RequestQueue queue;
+
+    private void getDataFromServer(String url){
+        if (queue == null){
+            queue = new Volley().newRequestQueue(getActivity());
+        }
+        if (jsonArrayRequest == null){
+            jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    your_collection = MyParser.parseJsonArray(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //eror 
+                }
+            });
+        }
+        queue.add(jsonArrayRequest);
+
+    }
