b3999b25b47aec2f428adb29f16deb7b06d804e6
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-02-16 17:39:08 +0400

    Update use_svg.java

--- use_svg.java
+++ use_svg.java
@@ -2,13 +2,13 @@
 //gradle dependency
 //compile 'org.mapsforge:svg-android:0.4.3'
 public void setSVGIntoIamageView(ImageView img){
-	SVG svg = null;
+    SVG svg = null;
     try {
- 	       svg = SVGParser.getSVGFromResource(getResources(), R.raw.logo);
+ 	    svg = SVGParser.getSVGFromResource(getResources(), R.raw.logo);
         } catch (SVGParseException e) {
-        e.printStackTrace();
-    }
-	img.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+	    e.printStackTrace();
+    	}
+    img.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     Drawable drawable = svg.createPictureDrawable();
-	img.setImageDrawable(drawable);                                       
+    img.setImageDrawable(drawable);                                       
 }
