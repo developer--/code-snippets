8876c05a2ae22eb7766e77abf65b989ffefc5235
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-03-25 15:37:14 +0400

    bottom sheet

---
+++ bottom_sheet_example.java
@@ -0,0 +1,45 @@
+// save into preference
+<RelativeLayout
+        app:layout_behavior="android.support.design.widget.BottomSheetBehavior"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        xmlns:ratioIV="http://schemas.android.com/apk/res-auto"
+        android:background="@color/clinic_adapter_view_back_color"
+        app:behavior_hideable="true"
+        android:id="@+id/bottom_sheet"
+        >
+
+        //content here
+
+
+
+</RelativeLayout>
+
+//Java
+
+    /**
+     * BottomSheet init
+     */
+    private void initBottomSheet(){
+        bottomSheet = yourRootView.findViewById(R.id.bottom_sheet);
+        behavior = BottomSheetBehavior.from(bottomSheet);
+    }
+
+   /**
+     * შევამოწმოთ შიტის მდგომარეობა გახსნილია თუ არა
+     */
+    private void checkSheetBehavior(){
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    //your logic here
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    //your logic here
+            }
+        });
+    }
