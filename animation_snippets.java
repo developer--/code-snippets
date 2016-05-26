4cfdf72a007bb34cf214657e375f294c25bdff39
Your Name <jemo.mgebrishvili@gmail.com>
2016-05-26 17:45:47 +0400

    circular_rival animation

---
+++ animation_snippets.java
@@ -0,0 +1,53 @@
+
+/**
+* რივეალ ეფექტი გამოჩენისას
+*/
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    void enterReveal(View view) {
+        // previously invisible view
+
+        // get the center for the clipping circle
+        int cx = view.getMeasuredWidth() / 2;
+        int cy = view.getMeasuredHeight() / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(view.getWidth(), view.getHeight()) / 2;
+
+        // create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+
+        // make the view visible and start the animation
+        view.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+
+
+    //გაქრობა რივეალ ანიმაციით 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    void exitReveal(final View myView) {
+        // previously visible view
+        // get the center for the clipping circle
+        int cx = myView.getMeasuredWidth() / 2;
+        int cy = myView.getMeasuredHeight() / 2;
+
+        // get the initial radius for the clipping circle
+        int initialRadius = myView.getWidth() / 2;
+
+        // create the animation (the final radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                myView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // start the animation
+        anim.start();
+    }
